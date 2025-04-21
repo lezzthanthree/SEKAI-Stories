5b34fce5783101492dfca90809d8e7ef1a87a532
Smilie Pop <arisatominato48@gmail.com>
2025-04-21 16:12:56 +0800

    Halt Netlify's Deployment

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,13 @@ function App() {
         <main>
             <div className="vercel-halt">
                 <p>
-                    I have to halt Vercel's deployment due to exceeding the
+                    Netlify's deployment has been halted due to exceeding the
                     bandwidth limit.
                 </p>
-                <p>Please head to Netlify App while I fix the problem.</p>
-                <p>
-                    An update will be posted there once the issue is resolved~
-                </p>
-                <a href="https://sekai-stories.netlify.app/">
-                    <img src="/img/netlify.png" alt="" />
+                <p>Please head to Vercel App.</p>
+
+                <a href="https://sekai-stories.vercel.app/">
+                    <img src="/img/vercel.png" alt="Vercel" />
                 </a>
             </div>
         </main>
