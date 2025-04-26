362f9177d6e26f9e7433ac6509d479c9a0045ccf
Smilie Pop <arisatominato48@gmail.com>
2025-04-26 12:22:56 +0800

    Cloudflare pause

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,18 @@ function App() {
     return (
         <main>
             <div className="vercel-halt">
-                <h1>New Announcement!</h1>
-                <p>SEKAI Stories will be moving to another site!</p>
-
+                <h1>Not so fast!</h1>
+                <p>SEKAI Stories will launch on April 27th, 00:00 JST!</p>
+                <p>For now, see this Nene playing CHUNITHM.</p>
+                <p></p>
+                <iframe
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/a0cx1PVf1t4"
+                    title="YouTube video player"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                ></iframe>
                 <button
                     className="btn-regular btn-orange"
                     onClick={() =>
