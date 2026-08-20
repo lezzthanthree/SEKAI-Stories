7f811a3497130f7e91e0b9b4f0b60c584875df84
BubblyVaporeon <109556338+BubblyVaporeon@users.noreply.github.com>
2026-08-20 02:34:07 -0400

    Add Japanese translations to i18n initialization

--- src/utils/i18ninit.tsx
+++ src/utils/i18ninit.tsx
@@ -40,6 +40,7 @@ const i18nInit = () => {
         lng: "en",
         resources: {
             en: { translation: en },
+            ja: { translation: ja},
             pl: { translation: pl },
             zh: { translation: zh },
             es: { translation: es },
